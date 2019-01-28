a812d66148316df003fdc7c81d176c51dfa87f87
Chris Clarke <chris.clarke97@gmail.com>
2019-01-27 21:33:58 -0600

    Add admin view for courses data

--- ace/courses/admin.py
+++ ace/courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Department, Course, Section, Instructor
+
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(Instructor)
--- ace/courses/models.py
+++ ace/courses/models.py
@@ -5,12 +5,18 @@ class Department(models.Model):
     abbreviation = models.CharField(max_length=8, primary_key=True)
     full_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.full_name
+
 
 class Instructor(models.Model):
     # Auto Generated InstructorID
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    website = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    website = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Course(models.Model):
@@ -20,13 +26,16 @@ class Course(models.Model):
     course_title = models.CharField(max_length=255)
     credit_hours = models.IntegerField()
 
+    def __str__(self):
+        return self.department.abbreviation + " " + str(self.course_number)
+
 
 class Section(models.Model):
     # Auto Generated SectionID
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     available_seats = models.IntegerField()
     enrolled_students = models.IntegerField()
-    section_type = models.CharField(max_length=255)
+    section_type = models.CharField(max_length=255, blank=True)
     section_number = models.IntegerField()
     time_begin = models.TimeField()
     time_end = models.TimeField()
@@ -38,5 +47,8 @@ class Section(models.Model):
     saturday = models.BooleanField()
     sunday = models.BooleanField()
     room = models.CharField(max_length=255)
-    special_enrollment = models.CharField(max_length=255)
+    special_enrollment = models.CharField(max_length=255, blank=True)
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course.__str__() + " [" + str(self.section_number) + "]"
