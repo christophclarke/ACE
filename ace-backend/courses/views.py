dda8477bfadd848147cd7b4c8fa7b028b9d1422f
Chris Clarke <chris.clarke97@gmail.com>
2019-02-12 23:34:48 -0600

    Add basic indexer at base url

--- ace-backend/courses/views.py
+++ ace-backend/courses/views.py
@@ -1,6 +1,28 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Course, Department
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the courses index.")
+    return render(request, "courses/index.html")
+
+
+def department_list(request):
+    data = list(Department.objects.values_list("abbreviation", flat=True))
+    return JsonResponse(data, safe=False)
+
+
+def course_list(request, dept="all"):
+    ret_dict = {}
+    if dept == "all":
+        q_set = Department.objects.all()
+    else:
+        q_set = Department.objects.filter(abbreviation=dept)
+
+    for department in q_set:
+        dept_list = Course.objects.filter(department=department)
+        dept_list = [str(course) for course in dept_list]
+        ret_dict[department.abbreviation] = dept_list
+
+    return JsonResponse(ret_dict)
