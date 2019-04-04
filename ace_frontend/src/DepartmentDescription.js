dc6fe6258c1e9ea9fed64f4894a214f4f2f3e4a6
Chris Clarke <chris.clarke97@gmail.com>
2019-04-04 03:27:49 -0500

    Restrict Course Search to Authenticated Users

--- ace_frontend/src/DepartmentDescription.js
+++ ace_frontend/src/DepartmentDescription.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import CourseList from './CourseList';
+import {BrowserRouter as Router, Route, Link, Switch, Redirect} from "react-router-dom";
+import CourseSearch from './CourseSearch';
 import CourseDescription from "./CourseDescription";
 import axios from 'axios';
+import {Container} from "react-bootstrap";
 
 class DepartmentDescription extends Component {
     constructor(props) {
@@ -52,6 +53,11 @@ class DepartmentDescription extends Component {
     }
 
     render() {
+        if (!this.props.isAuthenticated) {
+            console.log("User cannot access user data -> not logged in");
+            return <Redirect to="/login"/>
+        }
+
         const info = this.state.courses.map((entry, index) => {
             return (
                 <li key={index}>
@@ -61,20 +67,22 @@ class DepartmentDescription extends Component {
         });
 
         return (
-            <div className="container">
+            <Container>
                 <h1>{this.state.departmentData['full_name']}</h1>
                 {/* <Link to="/departments">Go Back</Link> */}
                 <Route exact path={this.props.match.url + "/"} render={(props)=> (
-                    <CourseList url={this.props.url} department={this.props.department}/>
+                    <CourseSearch url={this.props.url} department={this.props.department}/>
                 )}/>
                 <Route path={this.props.match.url + "/:course"} render={(props) =>
                     <CourseDescription
+                        {...props}
+                        userData={this.props.userData}
                         url={this.props.url}
                         department={this.props.department}
                         course={props.match.params.course}
                         handleSectionAdd={(sectionId) => this.props.handleSectionAdd(sectionId)}
                     />} />
-            </div>
+            </Container>
         )
     }
 }
